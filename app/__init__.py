f9296e9369d08e91f843f1c58790c983617bda1a
misocho <misochobrian@gmail.com>
2018-12-16 12:03:49 -0500

    [finish #162688202] adds configurations for the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -10,4 +10,8 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+
+    from app.api.v1 import v1
+    app.register_blueprint(v1)
+    
     return app
